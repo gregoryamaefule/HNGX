5d3b9f496926cfc4f72f93d0f5e9e420122191d2
gregory amaefule <amaefulegregory@gmail.com>
2023-09-11 08:42:02 +0100

    Update index.js
    important functionality update

--- HNGStageOneTaskFE/index.js
+++ HNGStageOneTaskFE/index.js
@@ -8,21 +8,22 @@ function setTime(){
 
     const currentDay = days[now.getUTCDay()]
 
-    const currentYear = now.getFullYear().toString()
-    const currentDate = now.getDate().toString().padStart(2, "0")
-    const currentMonth = now.getMonth()
-    const currentHour = now.getUTCHours().toString().padStart(2, "0")
-    const currentMin = now.getUTCMinutes().toString().padStart(2, "0")
-    const currentSeconds = now.getUTCSeconds().toString().padStart(2, "0")
-    const currentMSeconds = now.getUTCMilliseconds().toString().padStart(3, "0")
+    // const currentYear = now.getFullYear().toString()
+    // const currentDate = now.getDate().toString().padStart(2, "0")
+    // const currentMonth = now.getMonth()
+    // const currentHour = now.getUTCHours().toString().padStart(2, "0")
+    // const currentMin = now.getUTCMinutes().toString().padStart(2, "0")
+    // const currentSeconds = now.getUTCSeconds().toString().padStart(2, "0")
+    // const currentMSeconds = now.getUTCMilliseconds().toString().padStart(3, "0")
 
-    const currentTime = now.getTime()
+    const currentTime = Date.now()
 
 
 
     UTCDay.textContent = `${currentDay}`
-    UTCTime.textContent = `${currentYear}-${currentMonth + 1}-${currentDate} ${currentHour}:${currentMin}:${currentSeconds}:${currentMSeconds}`
+    // UTCTime.textContent = `${currentYear}-${currentMonth + 1}-${currentDate} ${currentHour}:${currentMin}:${currentSeconds}:${currentMSeconds}`
 
+    UTCTime.textContent = `${currentTime}`
     setInterval(setTime, 1000)
 
 
